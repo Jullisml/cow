4815e5011f579dafb47b78304dc5e46fe958bc2c
Jullisml <93387247+Jullisml@users.noreply.github.com>
2021-12-20 20:27:28 +0800

    Update cow.py

--- cow.py
+++ cow.py
@@ -1,5 +1,5 @@
-import re
 import numpy as np
+import re
 
 file = open('hello.cow', 'r')
 rd = file.read()
@@ -10,32 +10,32 @@ stek = []
 dctnr = {}
 ixx = 0
 
-result = np.zeros(100)
+result = np.zeros(1000)
 
 for item in a:
     if item == 'MOO':
         stek.append(ixx)
     if item == 'moo':
         dctnr[ixx]=stek[len(stek)-1]
-        dctnr[stek.pop()]=ixx
-        ixx+=1
-        
+        dctnr[stek.pop()] = ixx
+    ixx += 1
+    
 K = 0
 i = 0
 
 while(i != len(a)):
- if a[i] == 'MoO':
-     result[K] += 1
- if a[i] == 'MOo':
-     result[K] -= 1
- if a[i] == 'moO':
-     K += 1
- if a[i] == 'mOo':
-     K -= 1
- if a[i] == 'OOM':
-     print(chr(int(result[K])))
- if a[i] == 'Moo':
-    if a[K] != 0:
+    if a[i] == 'MoO':
+        result[K] += 1
+    if a[i] == 'MOo':
+        result[K] -= 1
+    if a[i] == 'moO':
+        K += 1
+    if a[i] == 'mOo':
+        K -= 1
+    if a[i] == 'OOM':
+        print(chr(int(result[K])))
+    if a[i] == 'Moo':
+        if a[K] != 0:
             print(chr(int(result[K])))
     if a[i] == 'OOO':
         a[K] = 0
